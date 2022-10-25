8ffeb1432393e89993c326520a7e0bea744deaaa
Javanrgb <85636992+Javanrgb@users.noreply.github.com>
2022-10-25 21:20:10 +0300

    Add files via upload

---
+++ cypress/e2e/active-client.spec.cy.js
@@ -0,0 +1,19 @@
+describe("Test components on this page",()=>{
+  beforeEach(()=>{
+    cy.login("string:en","User","Userone@123","Kenyatta National Hospital")
+
+  })
+  it("test1-checks that the patient queue button is clickable",()=>{
+    cy.get(".fa-stethoscope")
+    .click()
+    .should("be.visible")
+
+    cy.get("#patientIdentifier")
+    .type("Dennis Michael{enter}")
+
+    cy.wait(500)
+    
+
+
+  })
+})
