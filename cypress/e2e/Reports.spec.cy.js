29437fee398cc71b54ef405fcd00b2630f0c90e7
Javanrgb <85636992+Javanrgb@users.noreply.github.com>
2022-10-25 20:52:07 +0300

    Update Reports.spec.cy.js

--- cypress/e2e/Reports.spec.cy.js
+++ cypress/e2e/Reports.spec.cy.js
@@ -1,5 +1,5 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe(`The user is able to see the labels and controls including text-boxes, buttons and labels on the Login Page and can interacted with.`, () => {
+  beforeEach(() => {
+    cy.visit("https://testkspras.intellisoftkenya.com/bahmni/home/index.html#/login")
+
+  });
