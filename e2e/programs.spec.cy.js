9d2f2215447707f1d5bc56fb259b8d57f0089c91
Javanrgb <85636992+Javanrgb@users.noreply.github.com>
2022-10-25 07:51:19 +0300

    Delete e2e directory

--- e2e/programs.spec.cy.js
+++
@@ -1,9 +0,0 @@
-describe("Test components on this page",()=>{
-  beforeEach(()=>{
-    cy.login("string:en","superman","Admin123","Nextgen Academy")
-    cy.dashboard();
-  })
-  it("checks if the Registration button is clickable and  fucntional",()=>{
-    cy.get('.icon-bahmni-program').click()
-    })
-  })
