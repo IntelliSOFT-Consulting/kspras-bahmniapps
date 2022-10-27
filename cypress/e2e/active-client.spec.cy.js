f1e918328958b5cc9b4c367d8d8b195f2d810703
Javanrgb <85636992+Javanrgb@users.noreply.github.com>
2022-10-27 15:59:57 +0300

    Update active-client.spec.cy.js

--- cypress/e2e/active-client.spec.cy.js
+++ cypress/e2e/active-client.spec.cy.js
@@ -12,8 +12,20 @@ describe("Test components on this page",()=>{
     .type("John Doe{enter}")
 
     cy.wait(500)
-    
 
+  })
+
+  it("test2-checks that the home dashboard is clickable and treatment tile details clickable",()=>{
+    cy.get(".fa-stethoscope")
+    .click()
+    .should("be.visible")
+
+    cy.get("#patientIdentifier")
+    .type("John Doe{enter}")
+
+    cy.wait(5000)
+
+    cy.get('.dashboard-treatment-section > .section-title > .title-link').click()
 
   })
 })
